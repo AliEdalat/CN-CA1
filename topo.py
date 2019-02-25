ce1f0b3b46dc9158b804ab52529ca8184921a4cb
amir <iamirranjbar@gmail.com>
2019-02-25 12:56:37 +0330

    Topology file has been added.

---
+++ topo.py
@@ -0,0 +1,27 @@
+from mininet.topo import Topo
+from mininet.link import TCLink
+
+class MyTopo( Topo ):
+    "Simple topology example."
+
+    def __init__( self ):
+        "Create custom topo."
+
+        # Initialize topology
+        Topo.__init__( self )
+
+        # Add hosts and switches
+        firstHost = self.addHost( 'h1' )
+        secondHost = self.addHost( 'h2' )
+        thirdHost = self.addHost( 'h3' )
+        forthHost = self.addHost( 'h4' )
+        switch = self.addSwitch( 's' )
+
+        # Add links
+        self.addLink( firstHost, switch, cls=TCLink, bw=15)
+        self.addLink( secondHost, switch, cls=TCLink, bw=15)
+        self.addLink( thirdHost, switch, cls=TCLink, bw=15)
+        self.addLink( forthHost, switch, cls=TCLink, bw=15)
+
+
+topos = { 'mytopo': ( lambda: MyTopo() ) }
